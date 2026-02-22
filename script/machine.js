fcaa3bb9126f1040a4ce26171e60e3ad6be149cf
krisnokumarghosh <krishnokumarghosh112@gmail.com>
2026-02-22 12:31:54 +0600

    added reject btn system

--- script/machine.js
+++ script/machine.js
@@ -14,6 +14,19 @@ function showOnly (id){
     selected.classList.remove('hidden')
 }
 
+function availableJobs(id){
+    const allDiv = document.getElementById('all-jb-li');
+    const intDiv = document.getElementById('int-jb-li');
+    const rejDiv = document.getElementById('rej-jb-li');
+
+    allDiv.classList.add('hidden');
+    intDiv.classList.add('hidden');
+    rejDiv.classList.add('hidden');
+
+    const selected = document.getElementById(id);
+    selected.classList.remove('hidden');
+}
+
 function btnColorMain (id){
     const allBtn = document.getElementById('all-btn');
     const mainInterBtn = document.getElementById('main-inter-btn');
@@ -35,12 +48,13 @@ function interBtn (id){
     const getParent = getBtn.parentNode.parentNode.parentNode;
     const change = getParent.querySelector('.statuss');
     change.innerText = 'INTERVIEW'
+     change.classList.remove('text-[#002C5C]');
+    change.classList.add('text-green-500');
     const parentInner = getParent.innerHTML;
+    getParent.querySelector('.statuss').classList.remove('statuss');
     
     
-    console.log(change)
-    
-   
+ 
     const interContainer = document.getElementById('interview');
 
     const newDiv = document.createElement('div');
@@ -54,8 +68,49 @@ function interBtn (id){
     interCountNum++;
     interCount.innerText = interCountNum;
 
-getParent.querySelector('.statuss').classList.remove('statuss');
-   
+    const intTitle = document.getElementById('int-title-count');
+    let intTitleNum = Number(intTitle.innerText);
+    intTitleNum++;
+    intTitle.innerText = intTitleNum;
 
+   const intNoJob = document.getElementById('int-no-job');
+   intNoJob.classList.add('hidden');
+  
+}
+
+
+function rejectBtn(id){
+    const getBtn = document.getElementById(id);
+    const getParent = getBtn.parentNode.parentNode.parentNode;
+    const change = getParent.querySelector('.statuss');
+    change.innerText = 'REJECTED'
+    change.classList.remove('text-[#002C5C]');
+    change.classList.add('text-red-500');
+    const parentInner = getParent.innerHTML;
+    getParent.querySelector('.statuss').classList.remove('statuss');
+       
     
+ 
+    const rejectContainer = document.getElementById('rejected');
+
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `${parentInner}`
+       
+
+    rejectContainer.appendChild(newDiv);
+
+    const rejectCount = document.getElementById('reject-count');
+    let rejectCountNum = Number(rejectCount.innerText);
+    rejectCountNum++;
+    rejectCount.innerText = rejectCountNum;
+
+
+    const rejTitle = document.getElementById('rej-title-count');
+    let rejTitleNum = Number(rejTitle.innerText);
+    rejTitleNum++;
+    rejTitle.innerText = rejTitleNum;
+
+
+    const rejNoJob = document.getElementById('rej-no-job');
+   rejNoJob.classList.add('hidden');
 }
